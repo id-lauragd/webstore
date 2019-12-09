a162a7e3d5f395cc78d6edda6a00557fe58d2935
Laura Gema Dimas <lauragemadimas@Lauras-iMac.local>
2019-12-09 06:43:22 -0500

    java

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+const products =[
+
+{
+    id: 001,
+    name:  `Lavander`,
+    price: `$22`,
+    quantity: 5,
+    description: `Hand Cream`,
+
+},{
+    id: 002,
+    name:  `Lavander`,
+    price: `$22`,
+    quantity: 3,
+    description: `Hand Cream`,
+
+
+},{
+    id: 003,
+    name:  `Lavander`,
+    price: `$22`,
+    quantity: 7,
+    description: `Hand Cream`,
+
+},{
+    id: 004,
+    name:  `Lavander`,
+    price: `$22`,
+    quantity: 8,
+    description: `Hand Cream`,
+
+},
+
+]
+
+document.getElementById('something');
+
+
+
