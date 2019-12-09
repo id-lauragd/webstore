96c803e6e899c6c629c3baf7564177d788bb1dee
Graphic <graphic@WFD204-27m.local>
2019-12-09 09:56:24 -0500

    js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
-
-const products =[
+const natureProduct =[
 
 {
     product: 001,
-    name:  `Lavander`,
+    name:  `lavander`,
     price: `$22`,
     quantity: 5,
     description: `Hand Cream`,
 
 },{
     product: 002,
-    name:  `Lavander`,
+    name:  `sage`,
     price: `$22`,
     quantity: 3,
     description: `Hand Cream`,
@@ -18,24 +17,40 @@ const products =[
 
 },{
     product: 003,
-    name:  `Lavander`,
+    name:  `lemon`,
     price: `$22`,
     quantity: 7,
     description: `Hand Cream`,
 
 },{
     product: 004,
-    name:  `Lavander`,
+    name:  `lavander`,
     price: `$22`,
     quantity: 8,
-    description: `Hand Cream`,
+    description: `mint`,
 
 }
 
 ]
 
-getproductsAsHtmlString(product); {
+funtion getnatureProductsAsHtmlString(products); {
+
+    return `<article class="product">
+                <div class="product">
+                <img class="productImg" src="img/lavander.png" >
+                <h2 class="productTitle">Lavander</h2>
+                <p class="ProductP">$20</p>
+                </div>
+            </article>`;
 
     
 
+}
 
+
+funtion rendernatureProduct(arrToRender) {
+
+const arrOfHtmlnatureProduct = arrToRender.map(getnatureProductsAsHtmlString);
+const arrOfHtmlnatureProduct = arrOfHtmlnatureProduct.join(`/n`);
+document.getElementById(`natureProduct`).innerHTML = strOfHtmlString);
+}
